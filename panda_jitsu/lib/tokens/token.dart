0ed7f48458529a21e7bdffffc1cb8a7c977bcedd
Justin Shaw <realjustinshaw@gmail.com>
2020-03-29 17:15:57 -0700

    replaced tray and added token class

--- panda_jitsu/lib/tokens/token.dart
+++ panda_jitsu/lib/tokens/token.dart
@@ -1,9 +1,17 @@
+import 'dart:ui';
+
+import 'package:panda_jitsu/card_color.dart';
+import 'package:panda_jitsu/element.dart';
 
 /// Tracks a single token object.
 class Token {
 
-	/// The Token's parent tray.
-	Tray tray;
+	/// A basic token constructor.
+	Token(CardColor c, Element e) {
+		
+	}
 
-	Token(this.tray, CardColor c, Element e)
+	void update(double t) {}
+
+	void render(Canvas c) {}
 }
